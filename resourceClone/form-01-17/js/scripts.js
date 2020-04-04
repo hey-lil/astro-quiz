e61184af57afa99a4511dc3986daef5b2b87ba16
hey-lil <lillian.r.taft@gmail.com>
2020-04-04 15:32:51 -0700

    big dump of initial work for repo

---
+++ resourceClone/form-01-17/js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $("form#questions").submit(function() {
+    event.preventDefault();
+
+  $(".astroBranch#aries").click (function() { 
+    $().removeClass();                                   
+    $().addClass();     
+   alert("jesus christ why");
+  )};
+  
+  // $().removeClass();
+      // $().addClass(".ariesBackground");
+      // $().show("form#questions");
+      // alert("jesushchrist why");
+
+      // event.preventDefault();
+  // });
+
+});  
